780326c7c0871e8e01d024f12512116f61ded28e
Zach Schrock <zschrock45@gmail.com>
2021-06-28 17:36:50 -0500

    initial commit

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+    public bool animate;
+    private GameObject mc;
+    bool attack;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mc = GameObject.Find("Dragoon");
+        speed = 0.001f;
+        animate = false;
+        animator = mc.GetComponent<Animator>();
+        attack = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        //float v = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            attack = !attack;
+        }
+
+
+        mc.transform.position = new Vector2(transform.position.x + (h * speed), 0);
+    }
+}
